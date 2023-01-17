74dd552cc2bdbc34c91bbbfa1113ca254a743295
fabestah <stargalla.fabian98@gmail.com>
2023-01-17 23:24:05 +0100

    Add .zshrc & zsh .config files (aliases, exports)

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Configs quick access
+alias nvimrc='nvim ~/.config/nvim/'
+alias lvimrc='nvim ~/.config/lvim/config.lua'
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# List every file colorized automatically
+alias ls='ls --color=auto'
+alias lsa='ls -a'
+
+# Confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# Git (lazygit)
+alias lg='lazygit'
+
+# For when keys break
+alias archlinx-fix-keys="sudo pacman-key --init && sudo pacman-key --populate archlinux && sudo pacman-key --refresh-keys"
+
+# systemd
+alias mach_list_systemctl="systemctl list-unit-files --state=enabled"
---
+++ zsh/.zshrc
@@ -0,0 +1,25 @@
+#!/bin/sh
+[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"
+
+# Source
+plug "$HOME/.config/zsh/aliases.zsh"
+plug "$HOME/.config/zsh/exports.zsh"
+
+# Plugins
+plug "zap-zsh/supercharge"
+plug "zsh-users/zsh-autosuggestions"
+plug "zsh-users/zsh-syntax-highlighting"
+plug "hlissner/zsh-autopair"
+plug "zap-zsh/fzf"
+plug "zap-zsh/vim"
+
+# History
+HISTFILE=~/.zsh_history
+
+# Prompt theme
+plug "zap-zsh/zap-prompt"
+
+# Keybinds
+bindkey -v
+
+export PATH="$HOME/.local/bin":$PATH
