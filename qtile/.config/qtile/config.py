a4c5b70e14f29f51bbb08c1d8f69cc8cb70b585c
fabestah <stargalla.fabian98@gmail.com>
2023-02-02 22:15:53 +0100

    Remove last remains of old config

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -1,105 +1,14 @@
-from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
-from libqtile.lazy import lazy
-
 from core import (
-        groups,
-        keys,
-        mod,
-        shift,
-        layouts,
-        floating_layout,
-
+    groups,
+    hooks,
+    keys,
+    mod,
+    shift,
+    layouts,
+    floating_layout,
+    screens,
+    widget_defaults,
 )
-
-from libqtile.dgroups import simple_key_binder
-
-TERMINAL = "kitty"
-BROWSER = "librewolf"
-FONT_MAIN = "ttf-hack-nerd"
-
-#  WIDGETS 
-
-# Default Widget Settings
-widget_defaults = dict(
-    font="ttf-hack-nerd",
-    fontsize=14,
-    padding=3
-    # background = colors[2]
-)
-extension_defaults = widget_defaults.copy()
-
-def init_widgets_list():
-    widgets_list = [
-            # Margin
-            widget.Spacer(
-                length = 12,
-                #foreground = colors[2],
-                #background = color[0]
-                ),
-            widget.CurrentLayout(),
-            widget.GroupBox(),
-            widget.Prompt(),
-            widget.WindowName(),
-            widget.Chord(
-                chords_colors={
-                    "launch": ("#ff0000", "#ffffff"),
-                },
-                name_transform=lambda name: name.upper(),
-            ),
-            widget.CheckUpdates(
-                distro = "Arch",
-                display_format = "Updates: {updates}"
-                ),
-            widget.Systray(),
-            #widget.CurrentLayoutIcon(
-            #    custom_icon_paths = ["/home/fabestah/.config/qtile/icons"]
-            #    foreground = colors[2],
-            #    background = colors[0],
-            #    padding = 0
-            #    scale = 0.7
-            #    ),
-            widget.CPU(
-                format = "{load_percent}%",
-                update_interval = 0.5
-                ),
-            widget.CheckUpdates(
-                update_interval = 1800,
-                distro = 'Arch_checkupdates',
-                display_format = 'Updates: {updates}',
-                mouse_callbacks = {"Button1": lambda: qtile.cmd_spawn(TERMINAL + ' -e sudo pacman -Syu')},
-                font = FONT_MAIN,
-                foreground = "ffffff",
-                #background = color[0],
-                colour_have_updates = "ffffff",
-                colour_no_updates = "ffffff",
-                padding = 5,
-                #decorations = [
-                #    BorderDecoration(
-                #        #colour = colors[5],
-                #        border_width = [0, 0, 2, 0],
-                #        padding_x = 5,
-                #        padding_y = None,
-                #    )
-                #],
-                ),
-            widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-            widget.QuickExit(),
-            ]
-    return widgets_list
-
-def init_widgets_screen1():
-    widgets_screen1 = init_widgets_list()
-    return widgets_screen1
-
-def init_screens():
-    return [Screen(top=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=20))]
-
-if __name__ in ["config", "__main__"]:
-    screens = init_screens()
-    #widgets_list = init_widgets_list()
-    widgets_screen1 = init_widgets_screen1()
-
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
@@ -108,11 +17,11 @@ bring_front_click = False
 cursor_warp = False
 auto_fullscreen = True
 focus_on_window_activation = "smart"
-reconfigure_screens = True
+# reconfigure_screens = True
 
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
-auto_minimize = True
+auto_minimize = False
 
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
