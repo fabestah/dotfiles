6882c65b327a252f7931a86ae3536b31c264319d
fabestah <stargalla.fabian98@gmail.com>
2023-02-27 18:44:35 +0100

    Create lazy_function.py & add 4 lazy functions [Qtile]
    Lazy functions:
    move_window_to_next_screen()
    move_window_to_prev_screen()
    move_focus_to_next_screen()
    move_focus_to_prev_screen()
    
    Helper function:
    move_window_to_screen()

--- qtile/.config/qtile/utils/lazy_functions.py
+++ qtile/.config/qtile/utils/lazy_functions.py
@@ -1,1 +1,56 @@
+from libqtile import qtile
+from libqtile.backend.base import Window
+from libqtile.config import Screen
+from libqtile.core.manager import Qtile
+from libqtile.lazy import lazy
 
+
+
+def move_window_to_screen(qtile: Qtile, window: Window, screen: Screen):
+    """Moves a window to a screen and focuses it, allowing you to move it
+    further if you wish."""
+    window.togroup(screen.group.name)
+    qtile.focus_screen(screen.index)
+    screen.group.focus(window, True)
+
+
+
+qtile: Qtile = qtile
+
+@lazy.function
+def move_window_to_next_screen(_):
+    """Moves a window to the next screen. Loops around the beginning and
+    end."""
+    index = qtile.current_screen.index
+    index = index + 1 if index < len(qtile.screens) - 1 else 0
+    move_window_to_screen(qtile, qtile.current_window, qtile.screens[index])
+
+
+
+@lazy.function
+def move_window_to_prev_screen(_):
+    """Moves a window to the previous screen. Loops around the beginning and
+    end."""
+    index = qtile.current_screen.index
+    index = index - 1 if index > 0 else len(qtile.screens) - 1
+    move_window_to_screen(qtile, qtile.current_window, qtile.screens[index])
+
+
+
+@lazy.function
+def move_focus_to_next_screen(_):
+    """Moves the focus to the next screen. Loops around the beginning and
+    end."""
+    index = qtile.current_screen.index
+    index = index + 1 if index < len(qtile.screens) - 1 else 0
+    qtile.focus_screen(qtile.screens[index].index)
+
+
+
+@lazy.function
+def move_focus_to_prev_screen(_):
+    """Moves the focus to the previous screen. Loops around the beginning and
+    end."""
+    index = qtile.current_screen.index
+    index = index - 1 if index > 0 else len(qtile.screens) - 1
+    qtile.focus_screen(qtile.screens[index].index)
