e0b1671e0b655bbae71628832ad34681114d32b8
fabestah <stargalla.fabian98@gmail.com>
2023-03-03 19:20:08 +0100

    [Feature] Add libreoffice_dialogues hook (Qtile)

--- qtile/.config/qtile/core/hooks.py
+++ qtile/.config/qtile/core/hooks.py
@@ -6,8 +6,20 @@ from libqtile import hook
 
 @hook.subscribe.startup_once
 def start_once():
+    """Calls autostart script."""
     home = os.path.expanduser("~")
     subprocess.call([home + "/.config/qtile/scripts/autostart.sh"])
+
+
+
+@hook.subscribe.client_new
+def libreoffice_dialogues(window):
+    """Defines libreoffice dialogue windows as floating windows."""
+    if((window.window.get_wm_class() == ('VCLSalFrame', 'libreoffice-calc')) or
+    (window.window.get_wm_class() == ('VCLSalFrame', 'LibreOffice 3.4'))):
+        window.floating = True
+
+
 
 # @hook.subscribe.client_new
 # async def client_new(client):
