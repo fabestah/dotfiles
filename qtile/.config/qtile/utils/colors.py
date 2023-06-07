f38efbe62217368e23a8807fc80153652c3b263b
fabestah <stargalla.fabian98@gmail.com>
2023-06-07 18:06:24 +0200

    refactor: Migrate colors.py to use Variables class

--- qtile/.config/qtile/utils/colors.py
+++ qtile/.config/qtile/utils/colors.py
@@ -1,13 +1,15 @@
 import json
 
 from utils import dir
-from utils.variables import settings
+from utils.variables import var
 
-colorscheme = {"catppuccin": "catppuccin.json"}.get(
-    settings["colorscheme"], "catppuccin.json"
+
+colorscheme = var.settings.get("theme", {}).get(
+    "colorscheme", "catppuccin.json"
 )
 
 path = f"{dir.get()}/utils/colorscheme/{colorscheme}"
+
 
 with open(path, "r") as file:
     color = json.load(file)
