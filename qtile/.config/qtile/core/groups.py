cee28aaf4b1029a52f6affc36aab8ed2b99a18d7
fabestah <stargalla.fabian98@gmail.com>
2023-06-08 23:09:46 +0200

    refactor: Remove deprecated workspaces list

--- qtile/.config/qtile/core/groups.py
+++ qtile/.config/qtile/core/groups.py
@@ -61,17 +61,6 @@ workspaces = [
 ]
 
 
-# workspaces = [
-#    {"name": workspace_names[0], "key": "1", "matches": [Match(wm_class="firefox")], "lay": "monadtall"},
-#    {"name": workspace_names[1], "key": "2", "matches": [Match(wm_class="code-oss")], "lay": "monadtall"},
-#    {"name": workspace_names[2], "key": "3", "matches": [], "lay": "monadtall"},
-#    {"name": workspace_names[3], "key": "4", "matches": [Match(wm_class="discord")], "lay": "monadtall"},
-#    {"name": workspace_names[4], "key": "5", "matches": [Match(wm_class="Spotify")], "lay": "monadtall"},
-#    {"name": workspace_names[5], "key": "6", "matches": [], "lay": "monadtall"},
-#    {"name": workspace_names[6], "key": "7", "matches": [Match(wm_class="cypress")], "lay": "monadtall"},
-# ]
-
-
 groups = [Group(i) for i in "123456789"]
 
 
