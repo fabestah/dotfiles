5a1f9b46c79cc6c6231cadcee78686387e1e22fe
fabestah <stargalla.fabian98@gmail.com>
2023-06-10 22:55:59 +0200

    config: Set QUOTING_STYLE to show filenames without quotes
    Set QUOTING_STYLE in order to display filenames without quotes when using the ls command.

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,16 +1,20 @@
 #!/bin/sh
 HISTSIZE=1000000
 SAVEHIST=1000000
+
 export EDITOR="nvim"
 export TERMINAL="kitty"
 export BROWSER="brave"
 export PATH="$HOME/.local/bin":$PATH
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
+
 export PATH=$HOME/.cargo/bin:$PATH
 export PATH=$HOME/.local/share/go/bin:$PATH
 export GOPATH=$HOME/.local/share/go
 export PATH=$HOME/.fnm:$PATH
 export PATH="$HOME/.local/share/neovim/bin":$PATH
+
+export QUOTING_STYLE=literal
 export $(dbus-launch) # keyring functionality for codium github login
 eval "$(zoxide init zsh)"
