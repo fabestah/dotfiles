f9cecdee3bc2969ec2a635220a55dccfee6f596b
fabestah <stargalla.fabian98@gmail.com>
2023-06-25 00:06:13 +0200

    refactor: Migrate functions to Functions class

--- qtile/.config/qtile/utils/lazy_functions.py
+++ qtile/.config/qtile/utils/lazy_functions.py
@@ -5,56 +5,65 @@ from libqtile.core.manager import Qtile
 from libqtile.lazy import lazy
 
 
-def move_window_to_screen(qtile: Qtile, window: Window, screen: Screen):
-    """Moves a window to a screen and focuses it, allowing you to move it
-    further if you wish."""
-    window.togroup(screen.group.name)
-    qtile.focus_screen(screen.index)
-    screen.group.focus(window, True)
+class Functions:
+    qtile: Qtile = qtile
 
+    @staticmethod
+    def move_window_to_screen(qtile: Qtile, window: Window, screen: Screen):
+        """Moves a window to a screen and focuses it, allowing you to move it
+        further if you wish."""
+        window.togroup(screen.group.name)
+        qtile.focus_screen(screen.index)
+        screen.group.focus(window, True)
 
-qtile: Qtile = qtile
+    @staticmethod
+    @lazy.function
+    def move_window_to_next_screen(_):
+        """Moves a window to the next screen. Loops around the beginning and
+        end."""
+        index = Functions.qtile.current_screen.index
+        index = index + 1 if index < len(Functions.qtile.screens) - 1 else 0
+        Functions.move_window_to_screen(
+            Functions.qtile,
+            Functions.qtile.current_window,
+            Functions.qtile.screens[index],
+        )
 
+    @staticmethod
+    @lazy.function
+    def move_window_to_prev_screen(_):
+        """Moves a window to the previous screen. Loops around the beginning and
+        end."""
+        index = Functions.qtile.current_screen.index
+        index = index - 1 if index > 0 else len(Functions.qtile.screens) - 1
+        Functions.move_window_to_screen(
+            Functions.qtile,
+            Functions.qtile.current_window,
+            Functions.qtile.screens[index],
+        )
 
-@lazy.function
-def move_window_to_next_screen(_):
-    """Moves a window to the next screen. Loops around the beginning and
-    end."""
-    index = qtile.current_screen.index
-    index = index + 1 if index < len(qtile.screens) - 1 else 0
-    move_window_to_screen(qtile, qtile.current_window, qtile.screens[index])
+    @staticmethod
+    @lazy.function
+    def move_focus_to_next_screen(_):
+        """Moves the focus to the next screen. Loops around the beginning and
+        end."""
+        index = Functions.qtile.current_screen.index
+        index = index + 1 if index < len(Functions.qtile.screens) - 1 else 0
+        Functions.qtile.focus_screen(Functions.qtile.screens[index].index)
 
+    @staticmethod
+    @lazy.function
+    def move_focus_to_prev_screen(_):
+        """Moves the focus to the previous screen. Loops around the beginning and
+        end."""
+        index = Functions.qtile.current_screen.index
+        index = index - 1 if index > 0 else len(Functions.qtile.screens) - 1
+        Functions.qtile.focus_screen(Functions.qtile.screens[index].index)
 
-@lazy.function
-def move_window_to_prev_screen(_):
-    """Moves a window to the previous screen. Loops around the beginning and
-    end."""
-    index = qtile.current_screen.index
-    index = index - 1 if index > 0 else len(qtile.screens) - 1
-    move_window_to_screen(qtile, qtile.current_window, qtile.screens[index])
-
-
-@lazy.function
-def move_focus_to_next_screen(_):
-    """Moves the focus to the next screen. Loops around the beginning and
-    end."""
-    index = qtile.current_screen.index
-    index = index + 1 if index < len(qtile.screens) - 1 else 0
-    qtile.focus_screen(qtile.screens[index].index)
-
-
-@lazy.function
-def move_focus_to_prev_screen(_):
-    """Moves the focus to the previous screen. Loops around the beginning and
-    end."""
-    index = qtile.current_screen.index
-    index = index - 1 if index > 0 else len(qtile.screens) - 1
-    qtile.focus_screen(qtile.screens[index].index)
-
-
-@lazy.function
-def minimize_all_windows(qtile):
-    """Minimizes all windows in focused group."""
-    for win in qtile.current_group.windows:
-        if hasattr(win, "toggle_minimize"):
-            win.toggle_minimize()
+    @staticmethod
+    @lazy.function
+    def minimize_all_windows(_):
+        """Minimizes all windows in focused group."""
+        for win in Functions.qtile.current_group.windows:
+            if hasattr(win, "toggle_minimize"):
+                win.toggle_minimize()
