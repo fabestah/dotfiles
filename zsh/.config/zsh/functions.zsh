b28ed2560f8efa26ceb7e487054772c606338053
fabestah <stargalla.fabian98@gmail.com>
2023-07-06 21:06:48 +0200

    feat: Add rename_lower()
    Add rename_lower() function to recursively rename files and directories to lowercase within the current or specified directory.

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,1 +1,24 @@
 #!/bin/sh
+
+
+# Function to recursively rename files and directories to lowercase
+# Usage: rename_lower <directory>
+rename_lower() {
+    # Retrieve the directory path from the first argument
+    local directory="${1:-$(pwd)}"
+
+    # Loop through the files and directories in the given directory
+    for SRC in $(find "$directory" -depth); do
+        DST=$(dirname "$SRC")/$(basename "$SRC" | tr '[:upper:]' '[:lower:]')
+
+        # Check if renaming is required
+        if [ "$SRC" != "$DST" ]; then
+            # Perform the renaming
+            if [ ! -e "$DST" ]; then
+                mv -T "$SRC" "$DST"
+            else
+                echo "$SRC was not renamed"
+            fi
+        fi
+    done
+}
